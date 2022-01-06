6288271834ba1359318ce8d940d0c75bd0c1bdb0
Shreya Sharma <shreya1319.ss@gmail.com>
2022-01-06 19:36:48 +0530

    changes to detect faces

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
 ]).then(startFaceDetection);
 
-function startFaceDetection() {
+async function startFaceDetection() {
     const rectangularContainer = document.createElement('div');
     const facesDetected = document.createElement('div');
     rectangularContainer.style.position = 'relative';
+    const labeledFaceDescriptors = await loadPreDefinedImages();
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.5);
     uploadImage.addEventListener('change', async () => {
         const img = await faceapi.bufferToImage(uploadImage.files[0]);
         document.body.append(facesDetected);
@@ -22,19 +24,35 @@ function startFaceDetection() {
         rectangularContainer.append(img);
         const canvas = faceapi.createCanvasFromMedia(img);
         rectangularContainer.append(canvas);
-        const displaySize = {width:img.width, height: img.height};
+        const displaySize = {
+            width: img.width,
+            height: img.height
+        };
         faceapi.matchDimensions(canvas, displaySize);
         const detect = await faceapi.detectAllFaces(img).withFaceLandmarks().withFaceDescriptors();
         facesDetected.append(detect.length);
         const resizeDetections = faceapi.resizeResults(detect, displaySize);
-        resizeDetections.forEach( detect => {
-            const box = detect.detection.box;
-            const drawBox = new faceapi.draw.DrawBox(box, {label: 'Face'});
-            drawBox.draw(canvas);
-        })
-    })
+        const bestResults = resizeDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        bestResults.forEach((br, i) => {
+            const box = resizeDetections[i].detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: br.toString() })
+            drawBox.draw(canvas)
+        });
+    });
 }
 
 function loadPreDefinedImages() {
-    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Modi' ,'Thor', 'Tony Stark', 'Yogi'];
+    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Modi', 'Thor', 'Tony Stark', 'Yogi'];
+    return Promise.all(
+        labels.map(async label => {
+            const descriptions = []
+            for (let i = 1; i <= 2; i++) {
+                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/ss-1306/face-detection/main/labeled_images/${label}/${i}.jpg`)
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                descriptions.push(detections.descriptor)
+            }
+
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
+        })
+    )
 }
